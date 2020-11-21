e06fffb05896d3fdcc7b7423013030a418d89396
david <davidrguardiola@hotmail.es>
2020-11-21 21:19:46 +0100

    Merge origin/master
    Conflicts:
    	Manual/Manual/src/Ejemplo_Entornos_Graficos/Ejemplo_Lamina_Texto.java

--- src/serializacion1/Serializacion1.java
+++ src/serializacion1/Serializacion1.java
@@ -6,14 +6,12 @@
 package serializacion1;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 /**
  *
@@ -24,10 +22,12 @@ public class Serializacion1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException  {
 
         MClase obxeto1 = new MClase("ola", -7, 2.7E10);
         MClase obxeto2 = new MClase();
+
+        Object a = new Object();
 
         try {
 
@@ -38,6 +38,14 @@ public class Serializacion1 {
 
             escribir.close();
 
+            // casteo en dor pasos
+            
+            /*  a=ler.readObject();
+            obxeto2 = (MClase)a;                 
+            
+            //haria lo mismo que lo de abajo, casteo en un paso
+             */
+            
             obxeto2 = (MClase) ler.readObject();
 
             System.out.println(obxeto2.getNome());
